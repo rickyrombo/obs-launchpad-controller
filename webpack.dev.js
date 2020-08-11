66389fba5741457f3d47a6ee828641a603f29379
rickyrombo <rickyrombo@gmail.com>
2020-08-10 23:39:21 -0700

    remove dev server settings, we don't need them anymore?

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const HttpsSettings = require('./ssl-gen/https-config.gen.js');
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
-    devServer: {
-        // port: 443,
-        // contentBase: './build',
-        // index: 'index.html',
-        // https: HttpsSettings
-    }
 })
